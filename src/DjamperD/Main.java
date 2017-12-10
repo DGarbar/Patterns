9a3703f863c7761354b41acaaa05283983aae836
DjamperD <DimaGarbar@gmail.com>
2017-12-10 18:41:52 +0200

    Star t.

--- src/DjamperD/Main.java
+++ src/DjamperD/Main.java
@@ -1,11 +1,9 @@
 package DjamperD;
 
-import java.util.stream.Stream;
-
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("asdas");
+    System.out.println("asddsaas");
 
   }
 }
