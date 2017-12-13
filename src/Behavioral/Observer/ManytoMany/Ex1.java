2112359d7bb55a527aed803c844074e0cb5cbdf9
DjamperD <DimaGarbar@gmail.com>
2017-12-13 23:14:22 +0200

    ga

--- src/Behavioral/Observer/ManytoMany/Ex1.java
+++ src/Behavioral/Observer/ManytoMany/Ex1.java
@@ -20,7 +20,7 @@ public class Ex1 {
     uaNewsPublisher.registerObserver(radioChannel2);
     uaNewsPublisher.registerObserver(tvChannel);
 
-    ruNewsPublisher.setNews("Java best in the world");
+    ruNewsPublisher.setNews("Java -  best in the world");
     ruNewsPublisher.removeObserver(radioChannel1);
     ruNewsPublisher.setNews("Java is great");
 
