2288b3f86ff2046854a93bd4355e6ffb71573240
DjamperD <DimaGarbar49@gmail.com>
2018-02-02 17:43:18 +0200

    Create All in 1 md

--- src/Solution.java
+++ src/Solution.java
@@ -1,10 +1,63 @@
+import java.io.IOException;
+import java.nio.file.DirectoryStream.Filter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Solution {
 
   public static void main(String[] args) {
-    double n = 20;
-    for (int i = 0; i < Math.sqrt(n); i++) {
-      System.out.println("asd");
-      n -= i;
+    Path mainFile = Paths.get("main.md");
+    Path rootDir = Paths.get(System.getProperty("user.dir") + "//src");
+    Path readme = Paths.get(System.getProperty("user.dir") + "//README.md");
+
+    init(mainFile, readme);
+    dirWalk(rootDir, mainFile);
+
+  }
+
+  static void dirWalk(Path rootDir, Path mainFile) {
+    Filter<Path> filter = entry -> {
+      if (Files.isDirectory(entry)) {
+        dirWalk(entry, mainFile);
+        return false;
+      } else {
+        return Files.isDirectory(entry) || entry.toString().endsWith(".md") || entry.toString()
+          .endsWith("java");
+      }
+    };
+
+    try {
+      Files.newDirectoryStream(rootDir, filter).forEach(x -> writeToFile(mainFile, x));
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
+
+  static void writeToFile(Path copyTo, Path copyFrom) {
+    try {
+      if (copyFrom.toString().endsWith(".java")) {
+        Files.write(copyTo, "```java\n".getBytes(), StandardOpenOption.APPEND);
+        Files.write(copyTo, Files.readAllLines(copyFrom), StandardOpenOption.APPEND);
+        Files.write(copyTo, "```\n".getBytes(), StandardOpenOption.APPEND);
+      } else {
+        Files.write(copyTo, Files.readAllLines(copyFrom), StandardOpenOption.APPEND);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+  static void init(Path mainFile, Path readme) {
+    try {
+      Files.deleteIfExists(mainFile);
+      Files.copy(readme, mainFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 }
+
